65fe2d9f3730a9f6e188082309a7c9aec50df85e
kvcobb <kurtis.cobb@gmail.com>
2016-12-08 16:31:29 -0700

    Update bme_tempest_run.sh

--- scripts/bme_tempest_run.sh
+++ scripts/bme_tempest_run.sh
@@ -2,6 +2,12 @@ if [[ -n $1 || -n $2 ]]; then
 	echo "usage: bme_tempest_run.sh {smoke|persistent} <DIR>"
 	exit
 fi
+
+#KVC added if, pleasemove if you have a better location
+if [!-d /root/output]; then
+    mkdir -p /root/output;
+fi;
+
 cd ${2} 
 pip install -r requirements.txt 
 testr init
